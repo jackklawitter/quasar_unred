be3cc25ce5556a7f740ce309e97cb5a71b4fd973
Jack Klawitter <106770618+jackklawitter@users.noreply.github.com>
2023-09-12 20:49:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     packages=find_packages('quasar_unred'),
     package_dir={'': 'quasar_unred'},
     url='https://github.com/jackklawitter/quasar_unred',
+    download_url='https://github.com/jackklawitter/quasar_unred/archive/refs/tags/v0.6.tar.gz'
     keywords='Red Quasars',
     install_requires=[
           'numpy', 'scipy', 'astropy', 'dust_extinction', 'matplotlib'
